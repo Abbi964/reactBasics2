f2e703ab010cc30bb5496e45847636b715792386
abbi964 <abhinavthapliyal964@gmail.com>
2024-01-28 14:36:05 +0530

    Dynamic Styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,23 +5,38 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  // state for checking if input is valid or not
+  const [isValid, setIsValid] = useState(true)
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length >= 0){
+      setIsValid(true)
+    }
     setEnteredValue(event.target.value);
+  };
+  
+  const goalInputFoucusHandler = event => {
+    if (event.target.value.trim().length === 0){
+      setIsValid(false)
+    }
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0){
+      setIsValid(false);
+      return
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label style={{color : isValid ? 'black' : "red"}}>Course Goal</label>
+        <input style={{backgroundColor : isValid ? 'transparent' : "salmon", borderColor : isValid ? 'black' : "red"}} type="text" onFocus={goalInputFoucusHandler} onChange={goalInputChangeHandler} />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button className={`button ${!isValid ? 'invalid' : ''}`} type="submit">Add Goal</Button>
     </form>
   );
 };
